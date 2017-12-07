5efc6137d61dbd39b6187f0640495af9342b669a
Marco Schmidt <marcog.schmidt@gmail.com>
2017-12-07 01:16:28 -0600

    webpack-dev-server serves all content in both iFrame and inline modes

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,4 @@
-require('./style.css');
+// require('./www/style.css');
 document.write(require('./content.js'));
 
 [123].map((n) => n*n).forEach((n) => console.log(n));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
-var path = require('path');
+const path = require('path');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './entry.js',
+    entry: './src/entry.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.js'
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'build'),
+        contentBase: path.resolve(__dirname, 'build')
     },
     devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(['build']),
+        // Moves files
+        new TransferWebpackPlugin([
+            {from: 'www'},
+        ], path.resolve(__dirname, 'src')),
+    ],
+
     module: {
         rules: [
-            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            // {test: /\.css$/, use: ['style-loader', 'css-loader']},
             {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
         ]
     }
